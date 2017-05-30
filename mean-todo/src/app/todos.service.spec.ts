636b69ab295ce29c8d9a202c384176e811c3768f
Ajay NS <ajay.ns@outlook.com>
2017-05-31 00:45:24 +0530

    Add TodoSerive

---
+++ mean-todo/src/app/todos.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { TodosService } from './todos.service';
+
+describe('TodosService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodosService]
+    });
+  });
+
+  it('should ...', inject([TodosService], (service: TodosService) => {
+    expect(service).toBeTruthy();
+  }));
+});
