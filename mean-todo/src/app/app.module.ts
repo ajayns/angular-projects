a98b1f5a96dce0f494851eb038197557012ff1b1
Ajay NS <ajay.ns@outlook.com>
2017-05-31 00:13:53 +0530

    Add TodoComponent

--- mean-todo/src/app/app.module.ts
+++ mean-todo/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
