a1a9748e05b27ee146bc38f737ef304134147fbe
Ajay NS <ajay.ns@outlook.com>
2017-06-08 15:19:48 +0530

    github-search: Clean code and add comments

--- github-search/src/app/github.service.ts
+++ github-search/src/app/github.service.ts
@@ -10,16 +10,20 @@ export class GithubService {
 		console.log('Service Ready');
 	}
 	
+	// Get user by name
 	getUser() {
 		return this.http.get('http://api.github.com/users/'+this.username)
 			.map(res => res.json());
 	}
 
+	// Get user repos
 	getRepos() {
 		return this.http.get('http://api.github.com/users/'+this.username+'/repos')
 			.map(res => res.json());
 	}
 	
+
+	// Update username as user types in search
 	updateUser(username:string) {
 		this.username = username;
 	}
--- github-search/src/app/profile/profile.component.ts
+++ github-search/src/app/profile/profile.component.ts
@@ -7,11 +7,13 @@ import { GithubService } from '../github.service'
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+	// Declare variables
 	user:any = [];
 	repos:any = [];
 	username:string;
   
 	constructor(private githubService:GithubService) { 
+		// Initialize with false so that it doesn't display with *ngIf
 		this.user = false;
 	}
 
@@ -21,10 +23,12 @@ export class ProfileComponent implements OnInit {
 	searchUser() {
 		this.githubService.updateUser(this.username);
 		
+		// Get user calling githubService
 		this.githubService.getUser().subscribe( user => {
 			this.user = user;
 		});
 		
+		// Get repos calling githubService
 		this.githubService.getRepos().subscribe( repos => {
 			this.repos = repos;
 		});
